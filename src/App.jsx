d33b39540400379a6707cc07bd99e240be11495f
melisciar <84160185+melisciar@users.noreply.github.com>
2023-08-30 12:29:23 -0300

    Buscar cotización y mostrar resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
-import ImagenCripto from './img/imagen-criptos.png';
 import Formulario from './components/Formulario';
+import ImagenCripto from './img/imagen-criptos.png';
+import Resultado from './components/Resultado';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -40,12 +42,29 @@ const Heading = styled.h1`
 `;
 
 function App() {
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      const cotizarCripto = async () => {
+        const { moneda, cripto } = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado(resultado.DISPLAY[cripto][moneda]);
+      };
+      cotizarCripto();
+    }
+  }, [monedas]);
+
   return (
     <Contenedor>
       <Imagen src={ImagenCripto} alt="Imágenes criptomonedas" />
       <div>
         <Heading>Cotiza criptomonedas al Instante</Heading>
-        <Formulario />
+        <Formulario setMonedas={setMonedas} />
+        {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -22,7 +22,7 @@ const InputSubmit = styled.input`
   }
 `;
 
-const Formulario = () => {
+const Formulario = ({ setMonedas }) => {
   const [criptos, setCriptos] = useState([]);
   const [error, setError] = useState(false);
 
@@ -58,6 +58,10 @@ const Formulario = () => {
       return;
     }
     setError(false);
+    setMonedas({
+      moneda,
+      cripto,
+    });
   };
   return (
     <>
