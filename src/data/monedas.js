56ae2263570ff33283e84d9ec17c15b9b99245c3
melisciar <84160185+melisciar@users.noreply.github.com>
2023-08-30 10:54:01 -0300

    Initial commit

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: 'USD', nombre: 'Dólar estadounidense' },
+  { id: 'ARS', nombre: 'Peso argentino' },
+  { id: 'EUR', nombre: 'Euro' },
+  { id: 'GBP', nombre: 'Libra esterlina' },
+];
+
+export { monedas };
